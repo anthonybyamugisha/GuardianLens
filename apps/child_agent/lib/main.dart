1f9f7ec2d329f35df641a0b0a1ec82b124835316
habiba <habibahndawula@gmail.com>
2026-09-15 22:05:03 +0300

    modified

--- apps/child_agent/lib/main.dart
+++ apps/child_agent/lib/main.dart
@@ -1,121 +1,162 @@
 import 'package:flutter/material.dart';
 
+import 'screens/activity_screen.dart';
+import 'screens/alerts_screen.dart';
+import 'screens/child_home_screen.dart';
+import 'screens/my_apps_screen.dart';
+import 'screens/pairing_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/settings_screen.dart';
+import 'theme.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const GuardianLensChildApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GuardianLensChildApp extends StatelessWidget {
+  const GuardianLensChildApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: AppThemeMode.notifier,
+      builder: (context, mode, _) {
+        return MaterialApp(
+          title: 'GuardianLens',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode: mode,
+          home: const ChildAppRoot(),
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ChildAppRoot extends StatefulWidget {
+  const ChildAppRoot({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ChildAppRoot> createState() => _ChildAppRootState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ChildAppRootState extends State<ChildAppRoot> {
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
+  bool _paired = false;
+
+  void _goTo(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentIndex = index;
+    });
+  }
+
+  void _completePairing() {
+    setState(() {
+      _paired = true;
+      _currentIndex = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // ------------------------------------------------------------
+    // FIRST-TIME EXPERIENCE
+    // ------------------------------------------------------------
+    if (!_paired) {
+      return PairingScreen(
+        onPaired: _completePairing,
+      );
+    }
+
+    // ------------------------------------------------------------
+    // NORMAL CHILD APP
+    // ------------------------------------------------------------
+    final colors = context.colors;
+
+    Widget screen;
+
+    switch (_currentIndex) {
+      case 0:
+        screen = ChildHomeScreen(
+          onApps: () => _goTo(1),
+          onActivity: () => _goTo(2),
+          onAlerts: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => AlertsScreen(
+                  onBack: () => Navigator.pop(context),
+                ),
+              ),
+            );
+          },
+          onProfile: () => _goTo(3),
+        );
+        break;
+
+      case 1:
+        screen = MyAppsScreen(
+          onBack: () => _goTo(0),
+        );
+        break;
+
+      case 2:
+        screen = ActivityScreen(
+          onBack: () => _goTo(0),
+        );
+        break;
+
+      case 3:
+        screen = ChildProfileScreen(
+          onBack: () => _goTo(0),
+          onSettings: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ChildSettingsScreen(
+                  onBack: () => Navigator.pop(context),
+                ),
+              ),
+            );
+          },
+        );
+        break;
+
+      default:
+        screen = const SizedBox();
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: screen,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: _goTo,
+        backgroundColor: colors.cardBackground,
+        indicatorColor: colors.paleBlueSurface,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.apps_outlined),
+            selectedIcon: Icon(Icons.apps),
+            label: 'Apps',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.timeline_outlined),
+            selectedIcon: Icon(Icons.timeline),
+            label: 'Activity',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
